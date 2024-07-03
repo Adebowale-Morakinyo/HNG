4e663de48190f3210c9533f5153dd4a372f1342e
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-07-03 12:47:25 +0100

    Fetch Client's Location using ipinfo.io

--- hng_backend_task_1/app.py
+++ hng_backend_task_1/app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
+
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     visitor_name = request.args.get('visitor_name', 'Guest')
     client_ip = request.remote_addr
-    location = "New York"  # This will be updated later to fetch the actual location
-    temperature = 11  # This will be updated later to fetch the actual temperature
+
+    # Fetch location data
+    location_response = requests.get(f'http://ipinfo.io/{client_ip}/json')
+    location_data = location_response.json()
+    city = location_data.get('city', 'Unknown')
+    location = city
+
+    temperature = 11  # Placeholder for temperature fetching logic
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {location}"
 
     response = {
@@ -16,6 +24,7 @@ def hello():
         "greeting": greeting
     }
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
